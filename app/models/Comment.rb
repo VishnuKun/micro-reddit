c1397e8983c60552c20ba92088b27645025d38e6
Vishnu <vishnukun954AV@protonmail.com>
2023-11-20 11:44:57 +0530

    Added User model and changes on associations

--- app/models/Comment.rb
+++ app/models/Comment.rb
@@ -2,4 +2,5 @@ class Comment < ApplicationRecord
   validate :commenter, presence: true
   validate :comment, presence: true
   belongs_to :post
+  belongs_to :user
 end
--- app/models/Post.rb
+++ app/models/Post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
     validate :url, presence: true
-    has_many :posts
+    has_many :comments
     belongs_to :subreddit
+    belongs_to :user
 end
